66f13434c16c265294254b49f18310afe0e705bd
Kud Serhii <sergey.23snp.ua@gmail.com>
2020-11-29 17:09:54 +0200

    task 1

---
+++ hw2/main.js
@@ -0,0 +1,18 @@
+const citiesAndCountries = {
+	'Киев': 'Украина',
+	'Нью-Йорк': 'США',
+	'Амстердам': 'Нидерланды',
+	'Берлин': 'Германия',
+	'Париж': 'Франция',
+	'Лиссабон': 'Португалия',
+	'Вена': 'Австрия',
+};
+
+let result = [];
+
+for (const key in citiesAndCountries) {
+
+    result.push(`${key} - это ${citiesAndCountries[key]}`);
+}
+
+console.log(result); // task 1
