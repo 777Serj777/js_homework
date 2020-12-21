64674d646c72509ea64151d4513a6ab32e98ddb2
Kud Serhii <sergey.23snp.ua@gmail.com>
2020-12-21 11:33:46 +0200

    add hw5

--- main.js
+++ main.js
@@ -1,223 +1,69 @@
-class Student {
+let counter = ((num = 0) => (number) => num += number)();
+
+console.log(counter(3));
+console.log(counter(5));
+console.log(counter(228)); //result task 01
+
+let getUpdatedArr = ((arrElement = []) => (element) => (element === undefined) ? (arrElement = []) : (arrElement.push(element)) && arrElement)();
+
+getUpdatedArr(1);
+getUpdatedArr(5);
+console.log(getUpdatedArr({name: "Vasya"}));
+console.log(getUpdatedArr()); 
+console.log(getUpdatedArr(4)); //result task 02
+
+
+let getTime = (callTime => () => {
+
+    let time = new Date().getTime();    
+    let difference = callTime;
+
+    return   (callTime === undefined) ? (callTime = time) && 'Enabled' : (callTime = time) && ~~((callTime - difference) / 1000);
+
+})()
+
+console.log(getTime());
+
+setTimeout( () => {
+    console.log(getTime());
+}, 2000);  //result task 03
+
+
+
+window.onload = function(){
+
+    const time = 120; 
+
+    document.querySelector('.minute').innerHTML = (((time / 60 ) < 10)  ? '0'+ ~~(time / 60) : ~~(time / 60));
+    document.querySelector('.second').innerHTML = ( (time % 60 === 0) || (time % 60 < 10) ? '0' + time % 60 : time % 60 );
     
-    #id;
+    const timer = time => {
 
-    constructor(enrolle){
+        document.querySelector('#start').onclick = null;
 
-        this.#id = this.getId();
-        this.name = enrolle.name;
-        this.surname = enrolle.surname;
-        this.ratingPoint = enrolle.ratingPoint;
-        this.schoolPoint = enrolle.schoolPoint; 
-        this.isSelfPayment = this.selfPay();
-
-        Object.defineProperty(this, 'id',{
-            get: function(){
-                return this.#id;
+        setTimeout(function run() {
+    
+            if(time < 0 || time === undefined) {
+         
+                document.querySelector('#start').onclick = start;
+                console.log("Time End");
+                return;
             }
-        }) 
-    }
+        
+            document.querySelector('.minute').innerHTML = (((time / 60 ) < 10)  ? '0'+ ~~(time / 60) : ~~(time / 60));
+            document.querySelector('.second').innerHTML = ( (time % 60 === 0) || (time % 60 < 10) ? '0' + time % 60 : time % 60 );
 
-    getId() {
-        return ~~(Math.random() * 10**8);
-    }
-
-    selfPay() {
-
-        let point = 800;
-        let  budgetPlacesFull = false;
+            time--;
     
-        if(this.ratingPoint < point) return true;
+            setTimeout(run, 1000); 
+    
+        });
           
-        if(!budgetPlacesFull){
-    
-            for (let i = 0; i < Student.budgetPlaces.length; i++) {
-            
-                if(Student.budgetPlaces[i] === undefined){
-                    Student.budgetPlaces[i] = this;
-                    return false; 
-                }
-               
-            }
-    
-            budgetPlacesFull = true;
-        }
-       
-        return this.transferToBudget();
     }
-
-    transferToBudget() {
     
-        let arrBudgetPlaces = Student.budgetPlaces;
+    let start = () => timer(time);
     
-        arrBudgetPlaces.sort((objFirst, objSecond) => {
+    document.querySelector('#start').onclick =  start; //result task 04
     
-            if(objFirst.ratingPoint < objSecond.ratingPoint ) return 1;
-    
-            if(objFirst.ratingPoint > objSecond.ratingPoint ) return -1;
-    
-            if(objFirst.schoolPoint < objSecond.schoolPoint) return 1;
-    
-            if(objFirst.schoolPoint > objSecond.schoolPoint) return -1;
-    
-            return 0;
-        })
-    
-    {
-        // for (let i = 0; i < arrBudgetPlaces.length; i++) {
-            
-        //     for (let j = 0; j < arrBudgetPlaces.length - i - 1; j++) {
-    
-        //         if(arrBudgetPlaces[j].ratingPoint < arrBudgetPlaces[j + 1].ratingPoint){
-    
-        //             let tmp = arrBudgetPlaces[j];
-        //             arrBudgetPlaces[j] = arrBudgetPlaces[j + 1];
-        //             arrBudgetPlaces[j + 1] = tmp;
-    
-        //         }
-    
-        //         if(arrBudgetPlaces[j].ratingPoint === arrBudgetPlaces[j + 1].ratingPoint){
-    
-        //             if(arrBudgetPlaces[j].schoolPoint < arrBudgetPlaces[j + 1].schoolPoint){
-    
-        //                 let tmp = arrBudgetPlaces[j];
-        //                 arrBudgetPlaces[j] = arrBudgetPlaces[j + 1];
-        //                 arrBudgetPlaces[j + 1] = tmp;
-        //             }
-        //         }
-        //     }    
-        // }
-    }
-
-        let equalityTest = arrBudgetPlaces[arrBudgetPlaces.length - 1].ratingPoint - this.ratingPoint;
-
-        if(equalityTest > 0) return true;
-    
-        if(equalityTest === 0){
-            
-            equalityTest = arrBudgetPlaces[arrBudgetPlaces.length - 1].schoolPoint - this.schoolPoint;
-            
-            if(equalityTest > 0){
-                return true;
-            }
-        }
-        
-        arrBudgetPlaces[arrBudgetPlaces.length - 1].isSelfPayment = true;
-        arrBudgetPlaces[arrBudgetPlaces.length - 1] = this;
-        return false;
-     
-    }
-}
-
-Student.budgetPlaces = new Array(5);  
-Student.listOfStudents = [];
-
-Student.addStudent = function (enrolle)  {
-
- 
-    if(!(enrolle instanceof Array)){
-        this.listOfStudents.push(new Student(enrolle)); 
-        return;
-    }
-
-    for (const instance of enrolle) {
-        this.listOfStudents.push(new Student(instance));  
-    }
+};
   
-}
-
-Student.addStudent(enrolleArr);
-
-console.log(Student.budgetPlaces);
-console.log(Student.listOfStudents); //result task 01
-
-class CustomString{
-
-    reverse([...someString]){
-
-        let resultReverse = "";
-    
-        someString.forEach(element => {
-            resultReverse = element + resultReverse;
-        });
-
-        return resultReverse;
-    }
-    ucFirst([...someString]){
-
-        let result = "";
-    
-        if(someString[0].charCodeAt() >= 97 && someString[0].charCodeAt() <= 122){
-
-            someString.forEach(element => {
-               result += (result.length === 0)? String.fromCharCode(element.charCodeAt() - 32) : element;
-            });
-        }
-
-        else {
-            result = "range of characters a-z!"
-        }
-        
-        return result;
-    }
-    ucWords(someString){
-
-        let result = "";
-        let arrString = someString.split(' ');
-
-        arrString.forEach(element => {
-            result += (result.length === 0) ? new CustomString().ucFirst(element) : " " + new CustomString().ucFirst(element);    
-        });
-
-        return result;
-        
-    }
-}
-
-const myString = new CustomString();
-
-console.log(myString.reverse("qwerty"));
-console.log(myString.ucFirst('qwerty'));
-console.log(myString.ucWords('qwerty qwerty qwerty')); //result task 02
-
-class Validator{
-
-    checkIsEmail(someString){
-
-        let regexp = /^[\w.]*\@\b[a-z]{4,6}\b.\b[a-z]{2,3}\b/;
-
-        return regexp.test(someString);
-
-    }
-    checkIsDomain(someString){
-        let regexp = /^[a-zA-Z.]*.\b[a-z]{2,4}$/;
-
-        return regexp.test(someString);
-    }
-    checkIsDate(someString){
-
-        let reg = [/^\d\d\.[1-9][3-9]\.\d{4}$/, /^[3][2-9]\.[0-1]\d\.\d{4}$/, /^[0][0]\.[0-1]\d\.\d{4}$/, /^\d\d\.[0][0]\.\d{4}$/];
-
-        for (const element of reg) {
-            if(element.test(someString)) return false; 
-        }
-              
-        let regexp = /^[0-3]\d\.[0-1]\d\.\d{4}$/;
-
-        return regexp.test(someString);
-    }
-    checkIsPhone(someString){
-       return /^\+380\d{9}$/.test(someString) || /^\+38[(]0\d{2}[)]\d{7}$/.test(someString) || /^\+38[(]0\d{2}[)]\d{3}-\d{2}-\d{2}$/.test(someString);
-    }
-}
-
-var validator = new Validator();
-
-console.log(validator.checkIsDate('01.11.2021'));
-console.log(validator.checkIsDomain('www.google.com'));
-console.log(validator.checkIsPhone('+38(012)345-67-89'));
-console.log(validator.checkIsEmail('example.ua@gmail.com')); //result task 03
-
-
-
-
